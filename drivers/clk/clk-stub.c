3614171b2db2d5859ee774bcf17938ab90bc2e28
Vitalii Skorkin <nikroks@mainlining.org>
2024-07-25 15:11:40 +0500

    clk/stub: add SC7180 rpmhcc compatible
    Signed-off-by: Vitalii Skorkin <nikroks@mainlining.org>

--- drivers/clk/clk-stub.c
+++ drivers/clk/clk-stub.c
@@ -54,6 +54,7 @@ static const struct udevice_id stub_clk_ids[] = {
 	{ .compatible = "qcom,sm8250-rpmh-clk" },
 	{ .compatible = "qcom,sm8550-rpmh-clk" },
 	{ .compatible = "qcom,sm8650-rpmh-clk" },
+	{ .compatible = "qcom,sc7180-rpmh-clk" },
 	{ }
 };
 
